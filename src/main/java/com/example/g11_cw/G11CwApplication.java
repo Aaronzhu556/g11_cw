6d84cccc8193c2636ea48305be3d638bca1b3fdb
Aaronzhu <317663317@qq.com>
2023-05-09 14:07:48 +0100

    Initial commit

---
+++ src/main/java/com/example/g11_cw/G11CwApplication.java
@@ -0,0 +1,13 @@
+package com.example.g11_cw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G11CwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G11CwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g11_cw/G11CwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g11_cw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G11CwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
