83327bb6e1dbb640e187b3d37bf5e4597fcd5d3a
zyx <1770040579@qq.com>
2023-05-09 14:50:00 +0100

    改名

---
+++ src/main/java/com/example/g11_cw/Entity/Service.java
@@ -0,0 +1,17 @@
+package com.example.g11_cw.Entity;
+
+import lombok.Data;
+
+@Data
+public class Service {
+    private int seid;
+    private String pvid;
+    private String sedescription;
+    private String seprice;
+    private String seavailablity;
+    private String seavailable_time;
+    private String service_status;
+    private String service_description;
+    private String s_service_provider_id;
+    private String service_category;
+}
---
+++ src/main/java/com/example/g11_cw/Mapper/ServiceMapper.java
@@ -0,0 +1,14 @@
+package com.example.g11_cw.Mapper;
+
+import com.example.g11_cw.Entity.Service;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ServiceMapper {
+
+    List<Service> getAllService(String service_name, String service_area, String service_category);
+    int addService(Service service);
+    int updateService(Service service);
+}
