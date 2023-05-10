1abdf8b7010b616dd238a07a07550a5e7f54073b
zyx <1770040579@qq.com>
2023-05-10 21:23:57 +0100

    1.0版本🥳🥳🥳

--- src/main/java/com/example/g11_cw/Controller/ReviewController.java
+++ src/main/java/com/example/g11_cw/Controller/ReviewController.java
@@ -8,13 +8,12 @@ import com.example.g11_cw.Service.Interface.ReviewService;
 import com.example.g11_cw.Utils.TimeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@CrossOrigin
+@RestController
 @RequestMapping("/review")
 public class ReviewController {
 
